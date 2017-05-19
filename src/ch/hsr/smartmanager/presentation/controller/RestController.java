472311eb6b8754d76cd279bf7c8dbbf1effb6656
Andreas S. <andreas.stalder92@bluemail.ch>
2017-05-19 14:30:23 +0200

    Change Member Bugfixes

--- src/ch/hsr/smartmanager/presentation/controller/RestController.java
+++ src/ch/hsr/smartmanager/presentation/controller/RestController.java
@@ -143,11 +143,18 @@ public class RestController {
 		List<DeviceComponent> postMembers = new ArrayList<>();
 		for (int i = 0; i < value.length(); i++) {
 			try {
-				postMembers.add(deviceService.getComponent(value.getString(i)));
+
+				DeviceComponent item = deviceService.getGroup((value.getString(i)));
+				if(item == null) {
+					item = deviceService.getDevice((value.getString(i)));
+				}
+				postMembers.add(item);
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
 		}
+		
+		postMembers.removeAll(Collections.singleton(null));
 		
 		DeviceGroup group = deviceService.getGroup(id);
 		List<DeviceComponent> preMembers = group.getChildren();
@@ -155,10 +162,10 @@ public class RestController {
 		for (DeviceComponent comp : preMembers) {
 			if (!postMembers.contains(comp)) {
 				if(comp instanceof Device){
-					deviceService.removeDeviceFromGroup(comp.getId(), group.getId());
+					deviceService.removeDeviceFromGroup(group.getId(), comp.getId());
 				}
 				if(comp instanceof DeviceGroup){
-					deviceService.removeGroupFromGroup(comp.getId(), group.getId());
+					deviceService.removeGroupFromGroup(group.getId(), comp.getId());
 				}
 			}
 		}
@@ -169,7 +176,6 @@ public class RestController {
 					deviceService.addDeviceToGroup(group.getId(), comp.getId());
 				}
 				if(comp instanceof DeviceGroup){
-					System.out.println("Group to Group");
 					deviceService.addGroupToGroup(group.getId(), comp.getId());
 				}
 			}
